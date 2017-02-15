9903d1780fa897bb9f516f6d8952f9107391a0f2
kpereira <kpereira@cpqd.com.br>
2017-02-15 16:21:16 -0200

    Projeto Banco

---
+++ Banco/src/TesteContaEspecial.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteContaEspecial extends TesteContaCorrente{
+	
+	@Before
+	public void inicializaConta(){
+		cc = new ContaEspecial(100);
+		cc.depositar(200);
+	}
+	
+	@Test
+	public void saqueMaiorQueSaldo() {
+		int valorSacado = cc.sacar(350);
+		assertEquals(200, cc.saldo);
+		assertEquals(0, valorSacado);
+	}
+	
+	@Test
+	public void saqueMaiorQueSaldoDentroLimite() {
+		int valorSacado = cc.sacar(250);
+		assertEquals(-50, cc.saldo);
+		assertEquals(250, valorSacado);
+	}
+
+}
